3b209ea3af7060ebfd29db3508d78c2fad0afc4b
Michaelencisoyancce <apex3.0kmyro@email.com>
2023-08-28 18:06:03 -0500

    Agregados nuevos archivos y directorios

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $emailCount=1000;
+        User::factory($emailCount)->create();
+    }
+}
